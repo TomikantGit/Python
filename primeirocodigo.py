d0eaf1dd6459c711fa66f23e1922fa4f5aa35d3d
TomikantGit <pedrohenriquegitt@gmail.com>
2025-12-28 04:28:49 -0300

    mexendo com variavéis

---
+++ primeirocodigo.py
@@ -0,0 +1,18 @@
+preco = 10
+quantidade = 15
+lucro = preco * quantidade
+
+print(lucro)
+
+novo_preco = 150
+
+lucro = novo_preco * quantidade
+
+print(lucro)
+
+# tipos de variáveis: int, boolean, string e float
+# string = "nome"
+# int = X
+# float = X,X
+# Boolean = True, False
+
