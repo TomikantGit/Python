d920598499367665152fc06d9dd1b268ed6a7c1f
TomikantGit <pedrohenriquegitt@gmail.com>
2025-12-29 13:40:33 -0300

    Update primeiro_desafio.py

--- primeiro_desafio.py
+++ primeiro_desafio.py
@@ -1,1 +1,16 @@
 
+indice_arroba = email.find("@")
+
+dominio_email = email[indice_arroba:]
+
+print(dominio_email)
+
+indice_primeironome = nome.find(" ")
+
+primeiro_nome = nome[0:indice_primeironome+1]
+
+print(primeiro_nome)
+
+texto_final = f"O usuário {primeiro_nome}foi cadastrado com o dominio de email: {dominio_email}"
+
+print(texto_final)
