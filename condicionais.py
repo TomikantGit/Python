a7287e6bb1c0c01f97c54e6d79ea95e8dcbc7a12
TomikantGit <pedrohenriquegitt@gmail.com>
2025-12-31 01:37:53 -0300

    aprendendo condicionais, else e elif

---
+++ condicionais.py
@@ -0,0 +1,22 @@
+nome =  "Fernanda"
+idade = input(f"digite a idade da {nome} ")
+idade = int(idade)
+
+# para criar uma condição, esta deve ser a estrutura, else não recebe condição, significando literalmente: caso contrário, nunca esquecer de dar tab.
+
+if idade >= 18:
+    print("ela é maior de idade")
+else: 
+    print("ela é menor de idade")
+
+# funciona também com lista
+
+lista_nomes = ["João", "Kaynan", "Delrion", "Akilles", "Will"]
+
+nome_enviado = input("digite um nome para adicionar a lista ")
+
+if nome_enviado in lista_nomes:
+    print("esse nome já existe.")
+else: lista_nomes.append(nome_enviado)
+
+print(lista_nomes)
