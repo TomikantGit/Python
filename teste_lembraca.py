4cd5025f579a2f7f138320e801cb07e8816dce1b
TomikantGit <pedrohenriquegitt@gmail.com>
2026-02-04 12:43:54 -0300

    primeira parte

--- teste_lembraca.py
+++ teste_lembraca.py
@@ -1,1 +1,13 @@
-lista_pessoas = ["wita"]
+lista_pessoas = ["wita", "will", "lucas", "fernanda", "alice", "cleide"]
+
+# listas: ok
+
+nome_digitado = input("digite o nome da pessoa que ama")
+
+nome_digitado = nome_digitado.strip()
+nome_digitado = nome_digitado.capitalize()
+qtd_letras = len(nome_digitado)
+
+print(f"O Nome digitado foi {nome_digitado} e a quantidade de letras é: {qtd_letras} letras")
+
+# manipulação de strings: ok
